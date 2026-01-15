4a8e685fcc2f49c85f3cdb4896873a0a47879632
Dylan Plut <dylanplut@gmail.com>
2026-01-15 11:34:40 -0500

    first commit

---
+++ mychar.h
@@ -0,0 +1,18 @@
+/*
+--------------------------------------------------
+Project: a1q1
+File:    mychar.h
+Author:  Dylan Plut
+Version: 2026-01-15
+--------------------------------------------------
+*/
+#ifndef MYCHAR_H
+#define MYCHAR_H
+
+int mytype(char c);
+
+char case_flip(char c);
+
+int digit_to_int(char c);
+
+#endif
