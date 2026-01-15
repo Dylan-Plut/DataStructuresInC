b2034c7889b4dff201b1e4253e6c37ceb5395506
Dylan Plut <dylanplut@gmail.com>
2026-01-15 11:36:53 -0500

    first commit

--- mychar.c
+++ mychar.c
@@ -8,3 +8,14 @@ Version: 2026-01-15
 */
 #include "mychar.h"
 
+int mytype(char c) {
+    // your code
+}
+
+char case_flip(char c) {
+    // your code
+}
+
+int digit_to_int(char c) {
+    // your code
+}
